8606b1b1b24dc0dfeed1963fb9b4f7ca47fcf78c
Justin Tran <jctran@princeton.edu>
2017-08-25 12:48:25 -0700

    Commented code

--- MonteCarloNBA.py
+++ MonteCarloNBA.py
@@ -1,19 +1,19 @@
 import random
-for i in range(10):
-	consec_loss_season = 0
-	n = 1000000
 
-	for n in range(n):
-		last_game = 1
-		for i in range(82):
-			game = random.random()
-			if game < 0.80:
-				last_game = 1
-			elif game >= 0.80:
-				if last_game == 0:
-					consec_loss_season += 1
-					break
-				last_game = 0
+consec_loss_season = 0  # Seasons where there are consecutive losses
+n = 1000000  # Number of seasons to simulate
 
-	print('Probability that GSW does not lose consecutively: %s' %
-		  ((n - consec_loss_season) / n))
+for n in range(n):
+	last_game = 1  # Result of the last game "played". 1 = win, 0 = loss.
+	for i in range(82):  # Simulating each game in a season
+		game = random.random()
+		if game < 0.80:  # Win and continue to next game
+			last_game = 1
+		elif game >= 0.80:  # Lose and check if previous game was a loss
+			if last_game == 0:  # If last game was lost, count as a loss season
+				consec_loss_season += 1
+				break
+			last_game = 0
+
+print('Probability that GSW does not lose consecutively: %s' %
+	  ((n - consec_loss_season) / n))
